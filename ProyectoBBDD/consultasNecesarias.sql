7d9a0b2f463c33c9e89cef2b6217a9604cce3b3c
Dani13Ser <daniel.serkoz@sanviatorvalladolid.com>
2023-06-02 14:01:24 +0200

    viernes

--- ProyectoBBDD/consultasNecesarias.sql
+++ ProyectoBBDD/consultasNecesarias.sql
@@ -1,4 +1,7 @@
-select * from notafinal where dnialumno='80756322X';
-select * from notasboletin where dnialumno='80756322X';
+select * from notafinal;
+select * from notasboletin;
 select curso from modulo where ciclo='DAM' and abreviaturamodulo='BD';
 select (sum(valor)) from notasboletin where dnialumno='01921141C' and ciclo='IS' and modulo='AIE';
+use colegiosanviator;
+select * from matricula;
+select * from faltas;
--- ProyectoBBDD/trigger.sql
+++ ProyectoBBDD/trigger.sql
@@ -7,6 +7,7 @@ begin
 insert into horasprofesor values(new.dniprofesor, null);
 end;
 //
+
 create function calcularHoras(dni varchar(9))
 returns int
 begin 
@@ -15,6 +16,7 @@ select sum(horasSemana) into horas from modulo where dniProfesor=dni;
 return horas;
 end;
 //
+
 create trigger insercionModulo
 before insert 
 on modulo
@@ -27,6 +29,7 @@ set new.dniprofesor= null;
 end if;
 end;
 //
+
 create function faltas(dni varchar(9), ciclo varchar(3),modulo varchar(3),evaluacion int)
 returns int
 begin 
@@ -35,6 +38,7 @@ select count(*) into total from faltas where faltas.dni=dni and faltas.ciclo=cic
 return total;
 end;
 //
+
 create procedure valorTextual(num int)
 begin 
 if(num=1) then
@@ -52,6 +56,7 @@ update notafinal set valortextual ='MATRICULA DE HONOR' where valor=10;
 end if;
 end;
 //
+
 create trigger notaBoletin
 after insert
 on nota
@@ -78,6 +83,24 @@ call valortextual(1);
 end if;
 end;
 //
+
+create trigger notaBoletin2
+before update 
+on faltas
+for each row
+begin
+declare nota int;
+declare faltas int;
+set faltas=faltas(new.dni,new.ciclo,new.modulo,new.evaluacion);
+if(faltas+new.numfaltas>10) then
+set nota=(select sum(valor)/count(valor) from nota where new.dni=dniAlumno and tiponota='examen' and new.modulo=modulo and new.ciclo=ciclo and new.evaluacion=evaluacion);
+update notasboletin
+set valor= nota where new.dni=dniAlumno and new.modulo=modulo and new.ciclo=ciclo and new.evaluacion=evaluacion;
+call valortextual(1);
+end if;
+end;
+//
+
 create function contarEvaluaciones(dni varchar(9),ciclo varchar(3),modulo varchar(3))
 returns int
 begin
@@ -86,14 +109,18 @@ select count(*) into evaluaciones from notasboletin where dnialumno=dni and nota
 return evaluaciones;
 end;
 //
+
 create trigger notaFinal
 before insert 
 on matricula
 for each row 
 begin
+if(new.modulo!='PFG' and new.modulo!='PE') then
 insert into notaFinal values (new.dnialumno,new.ciclo,new.modulo,null,null);
+end if;
 end;
 //
+
 create trigger insertarNotaFinal
 after insert
 on notasBoletin
@@ -111,8 +138,10 @@ update notafinal set valor=(select avg(valor) from notasboletin where dnialumno=
 call valorTextual(2);
 else
 update notafinal set valor=(select avg(valor) from notasboletin where dnialumno=new.dnialumno and ciclo=new.ciclo and modulo=new.modulo) where dnialumno=new.dnialumno and ciclo=new.ciclo and modulo=new.modulo;
+call generarProyectoPracticas(new.dnialumno, new.ciclo);
 call valorTextual(2);
 end if;
+call alumnosEgresados(new.dnialumno);
 end;
 // 
 
@@ -120,14 +149,53 @@ create procedure generarProyectoPracticas(dni varchar(9),ciclo varchar(3))
 begin
 declare asignaturas int;
 declare aprobados int;
+declare notaProyecto int;
+declare notaPractica int;
+set notaProyecto=floor(rand()*11);
+set notaPractica=floor(rand()*11);
 select count(*) into asignaturas from notafinal where dnialumno=dni;
 select count(*) into aprobados from notafinal where dnialumno=dni and valor>=5;
 if(asignaturas=aprobados) then
-insert into notafinal values (dni,modulo,'PFG',floor(rand()*11),null);
-insert into notafinal values (dni,modulo,'PE',floor(rand()*11),null);
+insert into notasboletin values (dni,modulo,'PFG',notaProyecto,3,null);
+insert into notasboletin values (dni,modulo,'PE',notaPractica,3,null);
+call valorTextual(1);
+if(notaProyecto>5) then
+insert into notafinal values (dni,modulo,'PFG',notaProyecto,null);
+call valorTextual(2);
+else 
+set notaProyecto=floor(rand()*11);
+insert into notasboletin values (dni,modulo,'PFG',notaProyecto,4,null);
+insert into notafinal values (dni,modulo,'PFG',notaProyecto,null);
+call valorTextual(1);
 call valorTextual(2);
 end if;
+if(notaPractica>5) then
+insert into notafinal values (dni,modulo,'PE',notaProyecto,null);
+call valorTextual(2);
+else
+insert into notasboletin values (dni,modulo,'PE',notaProyecto,4,null);
+insert into notasboletin values (dni,modulo,'PE',notaPractica,null);
+call valorTextual(1);
+call valorTextual(2);
+end if;
+end if;
 end;
+//
 
-
+create procedure alumnosEgresados(dni varchar(9))
+begin
+declare aprobados int;
+declare fecha year;
+select count(*) into aprobados from notafinal where dnialumno=dni and modulo='PE' and valor>=5 or dnialumno=dni and modulo='PFG' and valor>=5;
+if(aprobados=2) then
+select count(*) into aprobados from notasboletin where dnialumno=dni and modulo='PE' and evaluacion=4 and valor>=5 or dnialumno=dni and modulo='PFG' and evaluacion=4 and valor>=5;
+select year(fechamatriculacion) into fecha from matricula where dnialumno=dni limit 1;
+if(aprobados=0) then
+insert into alumnosegresados values (dni,concat(fecha,'-08-31'),concat(fecha,'/',fecha+1));
+else
+insert into alumnosegresados values (dni,concat(fecha,'-09-01'),concat(fecha+1,'/',fecha+2));
+end if;
+end if;
+end;
+//
 
